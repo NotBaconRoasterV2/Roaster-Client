1cf620001dcef23bc2d6ff66185ad6b061bc67ab
BaconRoasterV2 <dylanoganda@gmail.com>
2026-04-15 10:41:59 +0300

    Update ModuleManager.java

--- src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
+++ src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
@@ -21,7 +21,7 @@ import me.alpha432.oyvey.features.modules.movement.StepModule;
 import me.alpha432.oyvey.features.modules.player.FastPlaceModule;
 import me.alpha432.oyvey.features.modules.player.NoFallModule;
 import me.alpha432.oyvey.features.modules.player.VelocityModule;
-import me.alpha432.oyvey.features.modules.player.FlightModule; // Added Import
+import me.alpha432.oyvey.features.modules.player.FlightModule;
 import me.alpha432.oyvey.features.modules.render.BlockHighlightModule;
 import me.alpha432.oyvey.util.traits.Jsonable;
 import me.alpha432.oyvey.util.traits.Util;
@@ -52,7 +52,7 @@ public class ModuleManager implements Jsonable, Util {
         register(new BlockHighlightModule());
         register(new NoFallModule());
         register(new KeyPearlModule());
-        register(new FlightModule()); // Registered FlightModule
+        register(new FlightModule()); // This must be here
 
         LOGGER.info("Registered {} modules", modules.size());
 
